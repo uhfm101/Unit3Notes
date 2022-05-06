a917f9ba2ca6f528b7183c0c08f9ec8ca44fc32c
uhfm <uhfm@fresnoU.org>
2022-05-06 14:46:50 -0700

    Day 2

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const cardList = document.querySelector('.cardList');
 let score = 0
-score++
+const content = document.querySelector(".content");
+const changeDiff = document.getElementById("ask");
 
 
 function addCard(value){
@@ -34,16 +35,75 @@ cardList.addEventListener("click", function(e){
     }
 
     if(e.target.classList.contains("active")){
-        e.target.classList.remove("active");
-        score +1
-        e.target.classList.add("inactive");
-        score +2
+         (e.target.classList.remove("active"))
+            score = score + 1;
+         (e.target.classList.add("inactive"))
+            score = score + 2;
+
         return
-        console.log(score)
+
     }
     e.target.remove();
     let children = cardList.children;
     if(children.length<1){
         clearInterval(interval);
+        const c = document.createElement("p");
+        c.innerHTML = `Your score is ${score}`
+        content.appendChild(c);
+
     }
+})
+
+changeDiff.addEventListener("click", function(e){
+score = 0
+
+    function addCard(value){
+        let card = document.createElement('div');
+        card.classList.add('card');
+        card.classList.add('active');
+        card.innerHTML = value;
+        cardList.appendChild(card);
+    }
+
+
+
+    function buildBoard(value){
+        for(let i = 0; i<12; i++){
+            addCard("starter")
+        }
+    }
+    buildBoard();
+
+    let interval =  setInterval(function(){
+        addCard(cardList.children.length +1)
+    }, 200)
+
+
+    cardList.addEventListener("click", function(e){
+        console.log(e.target);
+
+
+        if (e.target.matches(".cardList")){
+            return
+        }
+
+        if(e.target.classList.contains("active")){
+            (e.target.classList.remove("active"))
+            score = score + 1;
+            (e.target.classList.add("inactive"))
+            score = score + 2;
+
+            return
+
+        }
+        e.target.remove();
+        let children = cardList.children;
+        if(children.length<1){
+            clearInterval(interval);
+            const c = document.createElement("p");
+            c.innerHTML = `Your score is ${score}`
+            content.appendChild(c);
+
+        }
+    })
 })
