e95f56f1cd9a17cb6803e4d022d00d929a14b70f
uhfm <uhfm@fresnoU.org>
2022-05-23 14:42:26 -0700

    Revamp

--- index.js
+++ index.js
@@ -51,59 +51,60 @@ cardList.addEventListener("click", function(e){
         c.innerHTML = `Your score is ${score}`
         content.appendChild(c);
 
+         const b = document.createElement("button");
+         b.innerHTML = 'Difficulty: Hard'
+         content.appendChild(b);
+
+         b.addEventListener("click", function(e){
+            score = 0
+
+             function addCard(value){
+                 let card = document.createElement('div');
+                 card.classList.add('card');
+                 card.classList.add('active');
+                 card.innerHTML = value;
+                 cardList.appendChild(card);
+             }
+
+             function buildBoard(value){
+                 for(let i = 0; i<12; i++){
+                     addCard("starter")
+                 }
+             }
+             buildBoard();
+
+             let interval =  setInterval(function(){
+                 addCard(cardList.children.length +1)
+             }, 200)
+
+             cardList.addEventListener("click", function(e) {
+                 console.log(e.target);
+
+
+                 if (e.target.matches(".cardList")) {
+                     return
+                 }
+
+                 if (e.target.classList.contains("active")) {
+                     (e.target.classList.remove("active"))
+                     score = score + 1;
+                     (e.target.classList.add("inactive"))
+                     score = score + 2;
+
+                     return
+
+                 }
+                 e.target.remove();
+                 let children = cardList.children;
+                 if (children.length < 1) {
+                     clearInterval(interval);
+                     const c = document.createElement("p");
+                     c.innerHTML = `Your score is ${score}`
+                     content.appendChild(c);
+                 }
+             })
+
+        })
     }
 })
 
-changeDiff.addEventListener("click", function(e){
-score = 0
-
-    function addCard(value){
-        let card = document.createElement('div');
-        card.classList.add('card');
-        card.classList.add('active');
-        card.innerHTML = value;
-        cardList.appendChild(card);
-    }
-
-
-
-    function buildBoard(value){
-        for(let i = 0; i<12; i++){
-            addCard("starter")
-        }
-    }
-    buildBoard();
-
-    let interval =  setInterval(function(){
-        addCard(cardList.children.length +1)
-    }, 200)
-
-
-    cardList.addEventListener("click", function(e){
-        console.log(e.target);
-
-
-        if (e.target.matches(".cardList")){
-            return
-        }
-
-        if(e.target.classList.contains("active")){
-            (e.target.classList.remove("active"))
-            score = score + 1;
-            (e.target.classList.add("inactive"))
-            score = score + 2;
-
-            return
-
-        }
-        e.target.remove();
-        let children = cardList.children;
-        if(children.length<1){
-            clearInterval(interval);
-            const c = document.createElement("p");
-            c.innerHTML = `Your score is ${score}`
-            content.appendChild(c);
-
-        }
-    })
-})
